9a91d4d7d4474d2c78bac6f9436d4190bea14a54
bialemess <cl201288@g.unicamp.br>
2023-08-08 11:18:37 -0300

    a

---
+++ lib/my_cadastro.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class MyCadastro extends StatefulWidget {
+  const MyCadastro({super.key});
+
+  @override
+  State<MyCadastro> createState() => _MyCadastroState();
+}
+
+class _MyCadastroState extends State<MyCadastro> {
+
+String nome = "";
+int ra = 0;
+TextEditingController campoRa = TextEditingController();
+TextEditingController campoNome = TextEditingController();
+
+GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Column(
+        children: [
+          Form(
+            key: formKey,
+child: Column(children: [
+  TextFormField(
+controller: campoRa,
+validator: (value) {
+  if(value!.isEmpty){
+    return ("o Ra não pode ser vazio");
+  }
+  else if(int.parse(campoRa.text)<10){
+    //else if(campoRa.text.length<10){
+      
+      return("o RA deve ser maior que 10");
+    }
+  
+  return null;
+},
+
+  ),
+
+  TextFormField(
+controller: campoNome,
+validator: (value) {
+  if(value!.isEmpty){
+    return ("o nome não pode ser vazio");
+
+  }else{
+    if (campoNome.text.length<5){
+return("o nome precisa ter mais que 5 caracteres");
+    }
+  }
+  return null;
+},
+  )
+],
+
+  
+),
+          ),
+          ElevatedButton(
+onPressed: (){
+
+  if(formKey.currentState!.validate())
+  
+  {
+ra = int.parse(campoRa.text);
+nome = campoNome.text;
+setState(() {
+  
+
+
+});
+  }
+
+
+}, child: Text("Cadastrar")
+
+          ),
+          Text("Valores digitados: $ra, $nome"),
+        ],
+      )),
+    );
+  }
+}
